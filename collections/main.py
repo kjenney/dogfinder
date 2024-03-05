8dd90fa34e66d20c06895827bcfe30216d8b17e3
Ken Jenney <kjenney@gmail.com>
2024-03-04 21:51:31 -0500

    Save parquet to S3

--- collections/main.py
+++ collections/main.py
@@ -1,21 +1,31 @@
 from petpy import Petfinder
 import os
+import boto3
 from dotenv import load_dotenv
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 load_dotenv()
 
 api_key = os.getenv('PETFINDER_API_KEY')
 secret_key = os.getenv('PETFINDER_SECRET_KEY')
+bucket_name = os.getenv('BUCKET_NAME')
+parquet_file_name = os.getenv('PARQUET_FILE_NAME')
 
 pf = Petfinder(key=api_key, secret=secret_key)
-animals = pf.animals()
+dogs = pf.animals(animal_type='dog', status='adoptable', location='Boston, MA', distance=20,
+                  results_per_page=2, pages=1)["animals"] #, return_df=True)
+dog_list = []
+for dog in dogs:
+    desired_keys = ["id", "url", "name"]
+    new_dict = {key: dog[key] for key in desired_keys}
+    dog_list.append(new_dict)
+dog_pd = pd.json_normalize(dog_list)
+dog_table = pa.Table.from_pandas(dog_pd)
+pq.write_table(dog_table, f"/tmp/{parquet_file_name}")
 
-animal_ids = []
-for i in animals['animals'][0:3]:
-    animal_ids.append(i['id'])
-    
-animal_data = pf.animals(animal_id=animal_ids)
-print(animal_data)
+s3 = boto3.client("s3")
+s3.upload_file(f"/tmp/{parquet_file_name}", bucket_name, parquet_file_name)
 
-# Returning a pandas DataFrame of the first 150 animal results
-#animals = pf.animals(results_per_page=50, pages=3, return_df=True)
+
