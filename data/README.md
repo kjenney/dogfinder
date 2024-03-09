0bfdcd5b1585a602a7cf0b668604d62f05ab4e67
Ken Jenney <kjenney@gmail.com>
2024-03-08 20:29:27 -0500

    More progress

--- data/README.md
+++ data/README.md
@@ -3,8 +3,3 @@
 This includes a function to use Athena to query the Glue table that is generated after crawling the Parquet files.
 
 To get stack outputs use `sls info --verbose`
-
-## TODO
-
-Figure out how to pass the collections S3 bucket from the serverless output in collections to the parameter here
-
--- data/handler.py
+++ data/handler.py
@@ -1,58 +1,56 @@
 import boto3
 import pandas as pd
 import os
+import time
 
-BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
-#S3_PATH = "your/s3/path/to/parquet/file.parquet"  # Replace with your actual path to the
-#QUERY = "SELECT * FROM your_table_name"  # Replace with your desired Athena query
-#ROLE_ARN = "arn:aws:iam::your-account-id:role/your-role-name"  # Replace with your actual IAM#
-#REGION_NAME = "us-east-1"  # Adjust this as necessary to match the region of your S3 bucket
+GLUE_DATABASE_NAME = os.getenv('GLUE_DATABASE_NAME')
+RESULTS_BUCKET_NAME = os.getenv('RESULTS_BUCKET_NAME')
+ATHENA_WORKGROUP_NAME = os.getenv('ATHENA_WORKGROUP_NAME')
+GLUE_TABLE_NAME = os.getenv('GLUE_TABLE_NAME')
 
-# Create an Athena client using the AWS Session
 athena = boto3.client('athena')
+
+def run_query(query):
+    try:
+        query_execution_id = athena.start_query_execution(
+            QueryString=query,
+            QueryExecutionContext={
+                'Database': GLUE_DATABASE_NAME,
+                'Catalog': 'AwsDataCatalog'
+            },
+            ResultConfiguration={
+                'OutputLocation': f's3://{RESULTS_BUCKET_NAME}/athena-results/'
+            },
+            WorkGroup=ATHENA_WORKGROUP_NAME,
+        )['QueryExecutionId']
+
+        response = athena.get_query_execution(QueryExecutionId=query_execution_id)
+        while True:
+            status = response['QueryExecution']['Status']['State']
+
+            if status == 'SUCCEEDED':
+                break
+
+            elif status == 'RUNNING' or status == 'QUEUED':
+                # Wait for the query to finish executing
+                time.sleep(60)
+                response = athena.get_query_execution(QueryExecutionId=query_execution_id)
+
+            elif status == 'FAILED':
+                print("Query execution failed:")
+                return None
+
+        results_file = response['QueryExecution']['ResultSummary']['Location'] + '/' + response['QueryExecution']['ResultSummary']['S3Object']
+        df = pd.read_csv(results_file, header=0)
+
+        # Display the data for debugging purposes
+        print("Data retrieved from the query:")
+        print(df.head())
+
+    except Exception as e:
+        print(f"An error occurred while running the Athena query: {str(e)}")
 
 def handler(event, context):
     print("Running data handler")
-
-# def run_query(query):
-#     try:
-#         query_execution_id = athena.start_query_execution(
-#             QueryString=query,
-#             QueryExecutionContext={
-#                 'Database': BUCKET_NAME
-#             },
-#             ResultConfiguration={
-#                 'OutputLocation': f's3://{BUCKET_NAME}/athena-results/'
-#             },
-#             WorkGroup='default',
-#         )['QueryExecutionId']
-
-#         response = athena.get_query_execution(QueryExecutionId=query_execution_id)
-#         while True:
-#             status = response['QueryExecution']['Status']['State']
-
-#             if status == 'SUCCEEDED':
-#                 break
-
-#             elif status == 'RUNNING' or status == 'QUEUED':
-#                 # Wait for the query to finish executing
-#                 time.sleep(60)
-#                 response = athena.get_query_execution(QueryExecutionId=query_execution_id)
-
-#             elif status == 'FAILED':
-#                 print("Query execution failed:")
-#                 return None
-
-#         results_file = response['QueryExecution']['ResultSummary']['Location'] + '/' +
-# response['QueryExecution']['ResultSummary']['S3Object']
-#         df = pd.read_csv(results_file, header=0)
-
-#         # Display the data for debugging purposes
-#         print("Data retrieved from the query:")
-#         print(df.head())
-
-#     except Exception as e:
-#         print(f"An error occurred while running the Athena query: {str(e)}")
-
-# # Run the query
-# run_query(QUERY)
+    # Run the query
+    run_query(f"SELECT * FROM \"AwsDataCatalog\".\"{GLUE_DATABASE_NAME}\".\"{GLUE_TABLE_NAME}\" limit 10;")
